187dfb4f889e20725505c2a8b67bd87261d9461e
hoang anh <ngphuchoanganh@gmail.com>
2025-08-25 20:36:26 +0700

    feat: add some interactive with gum

--- run_once_after_bootstrap.sh
+++ run_once_after_bootstrap.sh
@@ -7,9 +7,7 @@
 set -e
 
 echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
-echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
 echo "                        BOOTSTRAPPING MACHINE"
-echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
 echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
 
 # --- Homebrew ---
@@ -22,49 +20,47 @@ else
   echo "Homebrew already installed."
 fi
 
-echo "Updating Homebrew and running Brewfile..."
-brew bundle
+# --- Gum ---
+if ! command -v gum &>/dev/null; then
+  brew install gum
+fi
+
+# Now that gum is installed, we can use it for the rest of the script.
+
+gum style --border thick --margin "1" --padding "1" --border-foreground 212 "››››››››››››››››››››››››››››››››››››››››››››››››››"
+gum style --bold --foreground 212 "                        STYLISH BOOTSTRAPPING"
+gum style --border thick --margin "1" --padding "1" --border-foreground 212 "››››››››››››››››››››››››››››››››››››››››››››››››››"
+
+gum style --border normal --margin "1" --padding "1" --border-foreground 212 "--- Homebrew Bundle ---"
+gum spin --spinner dot --title "Updating Homebrew and running Brewfile..." -- brew bundle
 
 # --- Rust ---
+gum style --border normal --margin "1" --padding "1" --border-foreground 212 "--- Rust ---"
 echo "Checking Rust..."
 if ! command -v rustup &>/dev/null; then
-  echo "Rustup not found. Installing..."
-  rustup-init -y
+  gum spin --spinner dot --title "Rustup not found. Installing..." -- rustup-init -y
 else
-  echo "Rust already installed."
+  gum style --foreground "0" --background "2" --padding "0 1" "Rust already installed."
 fi
 
 # --- Zsh ---
+gum style --border normal --margin "1" --padding "1" --border-foreground 212 "--- Zsh ---"
 echo "Checking Zsh..."
 if ! command -v zsh &>/dev/null; then
-  echo "Zsh not found. Installing..."
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if command -v apt &>/dev/null; then
-      sudo apt update
-      sudo apt install zsh -y
-    elif command -v dnf &>/dev/null; then
-      sudo dnf install zsh -y
-    else
-      echo "Could not find apt or dnf. Please install zsh manually."
-      exit 1
-    fi
-  else
-    echo "Unsupported OS for automatic zsh installation"
-  fi
+  gum spin --spinner dot --title "Zsh not found. Installing..." -- sudo apt update && sudo apt install zsh -y || sudo dnf install zsh -y
 else
-  echo "Zsh already installed."
+  gum style --foreground "0" --background "2" --padding "0 1" "Zsh already installed."
 fi
 
 # --- Zsh lugin ---
 if command -v zsh &>/dev/null; then
+  gum style --border normal --margin "1" --padding "1" --border-foreground 212 "--- Zsh Plugins ---"
   # --- Oh My Zsh ---
   echo "Checking for oh-my-zsh..."
   if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo "oh-my-zsh found. Pulling latest changes..."
-    git -C "$HOME/.oh-my-zsh" pull
+    gum spin --spinner dot --title "oh-my-zsh found. Pulling latest changes..." -- git -C "$HOME/.oh-my-zsh" pull
   else
-    echo "oh-my-zsh not found. Installing..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    gum spin --spinner dot --title "oh-my-zsh not found. Installing..." -- sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   fi
   # -- fzf-tab plugin ---
   echo "Checking for fzf-tab plugin..."
@@ -73,16 +69,23 @@ if command -v zsh &>/dev/null; then
   FZF_TAB_DIR="$ZSH_CUSTOM_DIR/plugins/fzf-tab"
 
   if [ ! -d "$FZF_TAB_DIR" ]; then
-    echo "fzf-tab not found. Cloning..."
-    git clone https://github.com/Aloxaf/fzf-tab "$FZF_TAB_DIR"
+    gum spin --spinner dot --title "fzf-tab not found. Cloning..." -- git clone https://github.com/Aloxaf/fzf-tab "$FZF_TAB_DIR"
   else
-    echo "fzf-tab already installed."
+    gum style --foreground "0" --background "2" --padding "0 1" "fzf-tab already installed."
   fi
 else
-  echo "Skipping installation because zsh is not installed."
+  gum style --foreground "3" --padding "0 1" "Skipping Zsh plugin installation because zsh is not installed."
 fi
 
-echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
-echo "                        BOOTSTRAP COMPLETE"
-echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
-echo "››››››››››››››››››››››››››››››››››››››››››››››››››"
+# Node package mangager
+gum style --border normal --margin "1" --padding "1" --border-foreground 212 "--- Node.js ---"
+if command -v fnm &>/dev/null; then
+  gum spin --spinner dot --title "fnm found. Installing latest LTS Node.js..." -- fnm install --lts --corepack-enabled
+else
+  gum style --foreground "3" --padding "0 1" "Skipping Node.js installation because fnm is not installed."
+fi
+
+gum style --border double --margin "1" --padding "1" --border-foreground 212 "››››››››››››››››››››››››››››››››››››››››››››››››››"
+gum style --bold --foreground 212 "                        BOOTSTRAP COMPLETE"
+gum style --border double --margin "1" --padding "1" --border-foreground 212 "››››››››››››››››››››››››››››››››››››››››››››››››››"
+
