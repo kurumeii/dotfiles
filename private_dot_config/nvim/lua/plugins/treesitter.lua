95b8342aa11c5c8992a322d9acb7fbf66dd8770d
Hoang Anh <ngphuchoanganh@gmail.com>
2025-09-04 21:30:19 +0700

    fix(nvim): Remove annoying notify

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -169,9 +169,11 @@ return {
 		"windwp/nvim-ts-autotag",
 		event = "InsertEnter",
 		opts = {
-			enable_close = true,
-			enable_rename = true,
-			enable_close_on_slash = false,
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = false,
+			},
 		},
 	},
 }
