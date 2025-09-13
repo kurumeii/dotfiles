1a51bfb292841ceb2199b12bbe84237ecc929201
Hoang Anh <ngphuchoanganh@gmail.com>
2025-09-13 21:07:04 +0700

    feat(nvim): conform plugin

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,40 +1,68 @@
 return {
 	{ -- Autoformat
 		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
+		event = { "BufReadPre" },
 		cmd = { "ConformInfo" },
 		keys = {
 			{
 				"<leader>cf",
 				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
+					require("conform").format()
 				end,
 				desc = "Code Format",
 			},
 		},
+		---@module "conform"
+		---@type conform.setupOpts
 		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
-				end
+			notify_on_error = true,
+			default_format_opts = {
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+				stop_after_first = true,
+			},
+			format_after_save = function(buf)
+				return {
+					bufnr = buf,
+					async = true,
+				}
 			end,
 			formatters_by_ft = {
+				markdown = { "markdownlint-cli2" },
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				json = { "biome" },
+				yaml = { "yamlfix" },
+				javascript = { "biome", "prettierd" },
+				typescript = { "biome", "prettierd" },
+				typescriptreact = { "biome", "prettierd" },
+				javascriptreact = { "biome", "prettierd" },
+				css = { "biome", "prettierd" },
+				scss = { "biome", "prettierd" },
+			},
+			formatters = {
+				biome = {
+					require_cwd = true,
+				},
+				stylua = {},
+				yamlfix = {},
+				["markdown-toc"] = {
+					condition = function(_, ctx)
+						for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+							if line:find("<!%-%- toc %-%->") then
+								return true
+							end
+							return false
+						end
+					end,
+				},
+				["markdownlint-cli2"] = {
+					condition = function(_, ctx)
+						local diag = vim.tbl_filter(function(d)
+							return d.source == "markdownlint"
+						end, vim.diagnostic.get(ctx.buf))
+						return #diag > 0
+					end,
+				},
 			},
 		},
 	},
