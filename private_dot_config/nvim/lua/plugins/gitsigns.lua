84a8f2ca49d7b48902a35e913d5469301f94e746
andrew nguyen <ngphuchoanganh@gmail.com>
2025-11-14 23:13:12 +0700

    refactor(nvim): Tweak ui for git

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -20,17 +20,17 @@ return {
 				topdelete = { text = mininvim.icons.git_signs.delete },
 				changedelete = { text = mininvim.icons.git_signs.change },
 			},
-			numhl = true,
+			numhl = false,
 			current_line_blame = true,
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-				delay = 1000,
+				delay = 3000,
 				ignore_whitespace = true,
 				virt_text_priority = 100,
 				use_focus = true,
 			},
-			current_line_blame_formatter = "<author> -> <summary> (<author_time:%R>)",
+			current_line_blame_formatter = "Coocked by <author> (<author_time:%R>) - <summary>",
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
 				local map = require("utils").map
