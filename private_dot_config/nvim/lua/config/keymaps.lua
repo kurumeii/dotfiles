20d4a79034b81cd16d59f73be5d5530001737a6e
andrew nguyen <ngphuchoanganh@gmail.com>
2025-11-17 22:55:46 +0700

    feat(nvim): add some util keymap

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,9 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<c-a>", "ggVG")
 vim.keymap.set("v", "p", [["_dP]])
--- vim: ts=2 sts=2 sw=2 et
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "<s-n>", "<s-n>zzzv")
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
+vim.keymap.set("n", "]t", ":tabnext<CR>")
+vim.keymap.set("n", "[t", ":tabprevious<CR>")
