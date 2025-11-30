ca1aa271b16bdc0e3cda40efa61135038135d442
andrew nguyen <ngphuchoanganh@gmail.com>
2025-11-30 13:51:18 +0700

    fix(minimax): Use j,k binding instead

--- private_dot_config/minimax/lua/plugins/mini/starter.lua
+++ private_dot_config/minimax/lua/plugins/mini/starter.lua
@@ -1,13 +1,5 @@
 local starter = require("mini.starter")
-starter.setup({
-	items = {
-		starter.sections.sessions(1, true),
-		starter.sections.recent_files(3, true, true),
-		starter.sections.builtin_actions(),
-	},
-
-	header = function()
-		return [[
+local header = [[
 					Meow! Here's your Neovim
            __..--''``---....___   _..._    __
  /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
@@ -15,7 +7,15 @@ starter.setup({
 / (_..-' // (< _     ;_..__               ; `' / ///
  / // // //  `-._,_)' // / ``--...____..-' /// / //
     ]]
-	end,
+
+starter.setup({
+	-- query_updaters = "",
+	items = {
+		starter.sections.sessions(1, true),
+		starter.sections.recent_files(3, true, true),
+		starter.sections.builtin_actions(),
+	},
+	header = header,
 	footer = function()
 		local elapsed_ns = vim.uv.hrtime() - vim.g.start_time
 		local elapsed_ms = elapsed_ns / 1e6
@@ -24,3 +24,20 @@ starter.setup({
 })
 local utils = require("config.utils")
 utils.map("n", utils.L("h"), MiniStarter.open, "Open Dashboard")
+
+vim.api.nvim_create_augroup("MiniStarterJK", { clear = true })
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniStarterOpened",
+	callback = function()
+		if vim.bo.filetype == "ministarter" then
+			local opts = { silent = true, buffer = true }
+			utils.map("n", "j", function()
+				MiniStarter.update_current_item("next")
+			end, "Dashboard: Next item", opts)
+			utils.map("n", "k", function()
+				MiniStarter.update_current_item("prev")
+			end, "Dashboard: Previous item", opts)
+		end
+	end,
+})
