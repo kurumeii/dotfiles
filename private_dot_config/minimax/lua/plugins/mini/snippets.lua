fa27470c20280832f178ef89595004bdec4dcdf1
Andrew Nguyen (D2) <andrew.nguyen1@techvify.com.vn>
2025-12-03 09:16:12 +0700

    fix(minimax): snippet

--- private_dot_config/minimax/lua/plugins/mini/snippets.lua
+++ private_dot_config/minimax/lua/plugins/mini/snippets.lua
@@ -1,20 +1,20 @@
-MiniDeps.add("rafamadriz/friendly-snippets")
+-- MiniDeps.add("rafamadriz/friendly-snippets")
 local lang_patterns = {
-	jsx = { "javascript/javascript.json", "javascript/react-es7.json" },
-	tsx = { "javascript/javascript.json", "javascript/typescript.json", "javascript/react-ts.json" },
+	jsx = { "javascript/javascript.json", "javascript/react.json" },
+	tsx = { "javascript/javascript.json", "javascript/react.json" },
 }
 
 local snippets, config_path = require("mini.snippets"), vim.fn.stdpath("config")
 snippets.setup({
 	snippets = {
+		snippets.gen_loader.from_file(config_path .. "/snippets/global.json"),
 		snippets.gen_loader.from_lang({
 			lang_patterns = lang_patterns,
 		}),
-		snippets.gen_loader.from_file(config_path .. "/snippets/global.json"),
-		-- snippets.start_lsp_server(),
+		snippets.start_lsp_server(),
 	},
 	mappings = {
-		expand = "",
+		expand = "<c-j>",
 	},
 	expand = {
 		select = function(snip, ins)
