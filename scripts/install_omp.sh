fd811ebad9c50b97605d54bc49de092b539fd10e
andrew nguyen <ngphuchoanganh@gmail.com>
2026-01-25 22:53:16 +0700

    add bunchs of things

--- scripts/install_omp.sh
+++ scripts/install_omp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Use oh-my-posh to install fonts
-if command -v oh-my-posh &> /dev/null; then
-		echo "Installing oh-my-posh fonts..."
-		FONTS=(
-				"CaskaydiaCove Nerd Font"
-				"FiraCode Nerd Font"
-				"JetBrainsMono Nerd Font"
-		)
-		for font in "${FONTS[@]}"; do
-        echo "Installing font: $font"
-        oh-my-posh font install "$font"
-    done
-		echo "oh-my-posh fonts installed successfully!"
+if command -v oh-my-posh &>/dev/null; then
+	echo "Installing oh-my-posh fonts..."
+	FONTS=(
+		"CascadiaCode"
+		"FiraCode"
+		"JetBrainsMono"
+	)
+	for font in "${FONTS[@]}"; do
+		echo "Installing font: $font"
+		oh-my-posh font install "$font"
+	done
+	echo "oh-my-posh fonts installed successfully!"
 else
-		echo "oh-my-posh command not found. Please ensure oh-my-posh is installed."
+	echo "oh-my-posh command not found. Please ensure oh-my-posh is installed."
 fi
