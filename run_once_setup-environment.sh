efb7f7b3704c9d330eee73fa45e14e63f19b6449
andrew nguyen <ngphuchoanganh@gmail.com>
2026-01-25 21:13:48 +0700

    use fish 🐟 shell instead

--- run_once_setup-environment.sh
+++ run_once_setup-environment.sh
@@ -2,4 +2,23 @@
 
 bash "${CHEZMOI_SOURCE_DIR}/import_dnf_plugins.sh"
 
+# Activeate mise
+if command -v mise &> /dev/null; then
+		echo "Activating mise..."
+		mise install -y
+		echo "mise activated successfully!"
+else
+		echo "mise command not found. Please ensure mise is installed."
+fi
 
+# Use oh-my-posh to install fonts
+if command -v oh-my-posh &> /dev/null; then
+		echo "Installing oh-my-posh fonts..."
+		for font in "${FONTS[@]}"; do
+        echo "Installing font: $font"
+        oh-my-posh font install "$font"
+    done
+		echo "oh-my-posh fonts installed successfully!"
+else
+		echo "oh-my-posh command not found. Please ensure oh-my-posh is installed."
+fi
